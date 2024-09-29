6c3463c97d97ef73e207d3741116d5c67e1017f1
ppppppsmash <pppppp.work@gmail.com>
2024-09-29 15:30:59 +0900

    feat: Create custom web component Widget

---
+++ src/web-component.tsx
@@ -0,0 +1,32 @@
+import { createRoot } from "react-dom/client";
+import { Widget } from "./components/Widget";
+
+export const normalizeAttribute = (attribute: string) => {
+  return attribute.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+};
+
+class WidgetWebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const props = this.getPropsFromAttributes();
+    if (this.shadowRoot) {
+      const root = createRoot(this.shadowRoot);
+      root.render(<Widget {...props} />);
+    }
+  }
+
+  getPropsFromAttributes() {
+    const props: Record<string, string> = {};
+    for (const { name, value } of this.attributes) {
+      props[normalizeAttribute(name)] = value;
+    }
+
+    return props;
+  }
+}
+
+export default WidgetWebComponent;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {
+      NODE_ENV: 'production',
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    lib: {
+      entry: './src/index.tsx',
+      name: 'widget',
+      // formats: ['es', 'umd'],
+      fileName: (format) => `widget.${format}.js`
+    },
+    target: 'esnext',
+  }
 })
